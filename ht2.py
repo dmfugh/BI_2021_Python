2614f2a646b291d1d0c412f0ab2b52521dc52525
dmfugh <91195667+dmfugh@users.noreply.github.com>
2021-10-04 20:34:52 +0300

    Add files via upload

---
+++ ht2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+try: 
+    c = int(input("Enter Celcium degrees "))
+    task = input("Enter degrees to convert: Fa, K ")
+    if task == "Fa":
+        f = (c*9/5) + 32
+        print (f)
+    elif task == "K":
+        k = c+273
+        print(k)
+    else: 
+        print("Wrong input")
+except:
+    print ("You have typed wrong data")
+
